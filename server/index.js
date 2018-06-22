d5bae4a9ad60d16cfeade579dc23ca9491d2911b
obravocedillo <oliverbravo45@gmail.com>
2018-06-21 19:19:08 -0500

    2.6.0

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.post('/login', function (req, res) {
+app.post('server/login', function (req, res) {
   res.send('Hello World!');
 });
 
